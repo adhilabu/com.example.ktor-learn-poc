430a786c2b7be330e37d66e60cda85182323c9bf
adhilabu <adhilabacker@gmail.com>
2024-06-05 23:14:47 +0530

    Initial commit

---
+++ src/main/kotlin/com/example/plugins/Routing.kt
@@ -0,0 +1,76 @@
+package com.example.plugins
+
+import io.ktor.server.application.*
+import io.ktor.server.http.content.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+import io.ktor.http.*
+import io.ktor.server.plugins.statuspages.*
+import com.example.model.*
+
+fun Application.configureRouting() {
+    install(StatusPages) {
+        exception<IllegalStateException> { call, cause ->
+            call.respondText("App in illegal state as ${cause.message}")
+        }
+    }
+
+    routing {
+        staticResources("/content", "mycontent")
+        get("/") {
+            call.respondText("Hello World!")
+        }
+
+        get("/test1") {
+            val text = "Hello from Ktor"
+            val type = ContentType.parse("text/html")
+            call.respondText(text, type)
+        }
+        get("/error-test") {
+            throw IllegalStateException("Too Busy")
+        }
+        get("/tasks") {
+            call.respondText(
+                contentType = ContentType.parse("text/html"),
+                text = """
+                <h3>TODO:</h3>
+                <ol>
+                    <li>A table of all the tasks</li>
+                    <li>A form to submit new tasks</li>
+                </ol>
+                """.trimIndent()
+            )
+        }
+        get("/tasks_list") {
+            val tasks = TaskRepository.allTasks()
+            call.respondText(
+                contentType = ContentType.parse("text/html"),
+                text = tasks.tasksAsTable()
+            )
+        }
+        get("/tasks/byPriority/{priority}") {
+            val priorityAsText = call.parameters["priority"]
+            if (priorityAsText == null) {
+                call.respond(HttpStatusCode.BadRequest)
+                return@get
+            }
+
+            try {
+                val priority = Priority.valueOf(priorityAsText)
+                val tasks = TaskRepository.tasksByPriority(priority)
+
+                if (tasks.isEmpty()) {
+                    call.respond(HttpStatusCode.NotFound)
+                    return@get
+                }
+
+                call.respondText(
+                    contentType = ContentType.parse("text/html"),
+                    text = tasks.tasksAsTable()
+                )
+            } catch(ex: IllegalArgumentException) {
+                call.respond(HttpStatusCode.BadRequest)
+            }
+        }
+    }
+}
